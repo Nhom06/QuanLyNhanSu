0f81f46b656e522dd479ebebe78ff5bfd610d19f
Nhom06 <lehuuhuysgu@gmail.com>
2015-11-04 22:34:02 +0700

    update

---
+++ QLNS_Service/QLNS_Service/IQLNS_Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+
+
+namespace QLNS_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IQLNS_Service" in both code and config file together.
+    [ServiceContract]
+    public interface IQLNS_Service
+    {
+        [OperationContract]
+        DataSet NhanVien();
+    }
+}
---
+++ QLNS_Service/QLNS_Service/QLNS_Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QLNS_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "QLNS_Service" in both code and config file together.
+    public class QLNS_Service : IQLNS_Service
+    {
+        public DataSet NhanVien()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select * from NhanVien", conn);
+            DataSet dsNhanVien = new DataSet();
+            da.Fill(dsNhanVien);
+            return dsNhanVien;        
+        }
+    }
+}
